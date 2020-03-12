9c62e044975d89bcea4881796970c0efb8d5e71a
carloscimi <57179052+carloscimi@users.noreply.github.com>
2020-03-12 20:58:40 +0000

    Deploy

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/aposteme-ui'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(__dirname + '/dist/apostem-ui/index.html');
+});
+
+// app.listen(4200);
+
+app.listen(process.env.PORT || 4200);
